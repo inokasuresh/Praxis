56b123e2e8b147708d6ea49682efdd1004ac2719
Inoka Suresh <rmisgeenath@gmail.com>
2019-08-13 15:40:36 +1000

    Changed couple of inappropriate variable names and references.

--- ITC205_201960_Asg2_Baseline_Code/BorrowBookControl.java
+++ ITC205_201960_Asg2_Baseline_Code/BorrowBookControl.java
@@ -3,40 +3,71 @@ import java.util.List;
 
 public class BorrowBookControl {
 
-	private BorrowBookUI burrowBookUI; // changed variable name UI to burrowBookUI. And all reference variable also
-										// changed.
+	/*
+	 * changed variable name UI to burrowBookUI. And all reference variable also
+	 * changed.
+	 */
+	private BorrowBookUI burrowBookUI;
 
-	private Library library; // changed variable name LIBRARY to library and Rename Class name library to
-								// Library. And all reference variable also changed.
-	private Member member; // changed variable name m to member and changed class name member to Member.
-							// And all reference variable also changed.
+	/*
+	 * changed variable name LIBRARY to library and Rename Class name library to
+	 * Library. And all reference variable also changed.
+	 */
+	private Library library;
 
-	private enum CONTROL_STATE {
+	/*
+	 * changed variable name m to member and changed class name member to Member.
+	 * And all reference variable also changed.
+	 */
+	private Member member;
+
+	/*
+	 * changed enum name CONTROL_STATE to ControlState. And all reference variable
+	 * also changed.
+	 */
+	private enum ControlState {
 		INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED
 	};
 
-	private CONTROL_STATE State;
+	/*
+	 * changed variable name State to controlState. And all reference variable also
+	 * changed.
+	 */
+	private ControlState controlState;
 
-	private List<book> PENDING;
-	private List<loan> COMPLETED;
-	private book BOOK;
+	/*
+	 * changed variable name PENDING to pendingBooksList. And wild card in List also
+	 * changed book to Book. And all reference variable also changed.
+	 */
+	private List<Book> pendingBooksList;
+	
+	/*
+	 * changed variable name COMPLETED to completedLoansList. And wild card in List also
+	 * changed loan to Loan. And all reference variable also changed.
+	 */
+	private List<Loan> completedLoansList;
+	
+	/*
+	 * changed variable name and class name
+	 */
+	private Book book;
 
 	public BorrowBookControl() {
 		this.library = library.INSTANCE();
-		State = CONTROL_STATE.INITIALISED;
+		controlState = ControlState.INITIALISED;
 	}
 
 	public void setUI(BorrowBookUI ui) {
-		if (!State.equals(CONTROL_STATE.INITIALISED))
+		if (!controlState.equals(ControlState.INITIALISED))
 			throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
 
 		this.burrowBookUI = ui;
 		ui.Set_State(BorrowBookUI.UI_STATE.READY);
-		State = CONTROL_STATE.READY;
+		controlState = ControlState.READY;
 	}
 
 	public void Swiped(int MEMMER_ID) {
-		if (!State.equals(CONTROL_STATE.READY))
+		if (!controlState.equals(ControlState.READY))
 			throw new RuntimeException("BorrowBookControl: cannot call cardSwiped except in READY state");
 
 		member = library.MEMBER(MEMMER_ID);
@@ -45,9 +76,9 @@ public class BorrowBookControl {
 			return;
 		}
 		if (library.MEMBER_CAN_BORROW(M)) {
-			PENDING = new ArrayList<>();
+			pendingBooksList = new ArrayList<>();
 			burrowBookUI.Set_State(BorrowBookUI.UI_STATE.SCANNING);
-			State = CONTROL_STATE.SCANNING;
+			controlState = ControlState.SCANNING;
 		} else {
 			burrowBookUI.Display("Member cannot borrow at this time");
 			burrowBookUI.Set_State(BorrowBookUI.UI_STATE.RESTRICTED);
@@ -56,7 +87,7 @@ public class BorrowBookControl {
 
 	public void Scanned(int bookId) {
 		BOOK = null;
-		if (!State.equals(CONTROL_STATE.SCANNING)) {
+		if (!controlState.equals(ControlState.SCANNING)) {
 			throw new RuntimeException("BorrowBookControl: cannot call bookScanned except in SCANNING state");
 		}
 		BOOK = library.Book(bookId);
@@ -68,49 +99,49 @@ public class BorrowBookControl {
 			burrowBookUI.Display("Book cannot be borrowed");
 			return;
 		}
-		PENDING.add(BOOK);
-		for (book B : PENDING) {
+		pendingBooksList.add(BOOK);
+		for (book B : pendingBooksList) {
 			burrowBookUI.Display(B.toString());
 		}
-		if (library.Loans_Remaining_For_Member(member) - PENDING.size() == 0) {
+		if (library.Loans_Remaining_For_Member(member) - pendingBooksList.size() == 0) {
 			burrowBookUI.Display("Loan limit reached");
 			Complete();
 		}
 	}
 
 	public void Complete() {
-		if (PENDING.size() == 0) {
+		if (pendingBooksList.size() == 0) {
 			cancel();
 		} else {
 			burrowBookUI.Display("\nFinal Borrowing List");
-			for (book B : PENDING) {
+			for (book B : pendingBooksList) {
 				burrowBookUI.Display(B.toString());
 			}
-			COMPLETED = new ArrayList<loan>();
+			completedLoansList = new ArrayList<loan>();
 			burrowBookUI.Set_State(BorrowBookUI.UI_STATE.FINALISING);
-			State = CONTROL_STATE.FINALISING;
+			controlState = ControlState.FINALISING;
 		}
 	}
 
 	public void Commit_LOans() {
-		if (!State.equals(CONTROL_STATE.FINALISING)) {
+		if (!controlState.equals(ControlState.FINALISING)) {
 			throw new RuntimeException("BorrowBookControl: cannot call commitLoans except in FINALISING state");
 		}
-		for (book B : PENDING) {
+		for (book B : pendingBooksList) {
 			loan LOAN = library.ISSUE_LAON(B, member);
-			COMPLETED.add(LOAN);
+			completedLoansList.add(LOAN);
 		}
 		burrowBookUI.Display("Completed Loan Slip");
-		for (loan LOAN : COMPLETED) {
+		for (loan LOAN : completedLoansList) {
 			burrowBookUI.Display(LOAN.toString());
 		}
 		burrowBookUI.Set_State(BorrowBookUI.UI_STATE.COMPLETED);
-		State = CONTROL_STATE.COMPLETED;
+		controlState = ControlState.COMPLETED;
 	}
 
 	public void cancel() {
 		burrowBookUI.Set_State(BorrowBookUI.UI_STATE.CANCELLED);
-		State = CONTROL_STATE.CANCELLED;
+		controlState = ControlState.CANCELLED;
 	}
 
 }
