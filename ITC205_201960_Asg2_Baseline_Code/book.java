4a5eae07d271b03406fd118666e18c1468ae5f8c
Pubudu Nuwan <rasiabc@gmail.com>
2019-08-14 17:39:19 +1000

    Modified the method signatures based on the standard

--- ITC205_201960_Asg2_Baseline_Code/book.java
+++ ITC205_201960_Asg2_Baseline_Code/book.java
@@ -2,11 +2,11 @@ import java.io.Serializable;
 
 
 @SuppressWarnings("serial")
-public class Book implements Serializable { //Change the class name based on the slandered 
+public class Book implements Serializable { //Change the class name based on the standard  
 	
 	private String title; // Change the variable name based on the standard
 	private String author; // Change the variable name based on the standard
-	private String isbnNo; // change CALLNO as isbnNo 
+	private String isbnNo; // change CALLNO as isbnNo to be more meaningful 
 	private int id; // Change the variable name based on the standard
 	
 	private enum State { AVAILABLE, ON_LOAN, DAMAGED, RESERVED }; //Change enum name base on the standard
@@ -18,77 +18,78 @@ public class Book implements Serializable { //Change the class name based on the
 		this.title = title; //Change the variable name based on the standard
 		this.isbnNo = isbnNo;//Change the variable name based on the standard
 		this.id = id; //Change the variable name based on the standard
-		this.State = State.AVAILABLE;//Change enum name based on the standard
+		this.state = State.AVAILABLE;//Change enum and class variable based on the standard
 	}
 	
 	public String toString() { 
 		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(ID).append("\n")
-		  .append("  Title:  ").append(TITLE).append("\n")
-		  .append("  Author: ").append(AUTHOR).append("\n")
-		  .append("  CallNo: ").append(CALLNO).append("\n")
-		  .append("  State:  ").append(State);
+		sb.append("Book: ").append(id).append("\n")//Change the class variable in to lower case
+		  .append("  Title:  ").append(title).append("\n")//Change the class variable in to lower case
+		  .append("  Author: ").append(author).append("\n")//Change the class variable in to lower case
+		  .append("  CallNo: ").append(isbnNo).append("\n")//chnage the class variable name to be more meaningful
+		  .append("  State:  ").append(state);//Change the class variable in to lower case
 		
 		return sb.toString();
 	}
 
-	public int getId() {//Change the return type and getter method
-		return id;//change the variable name
+	public int getId() {//Change the return type and getter method signature
+		return id;//change the variable name in to lowercase
 	}
 
-	public String getTitle() { //Change the return type and getter method
-		return title;//change the variable name
+	public String getTitle() { //Change the return type and getter method signature
+		return title;//change the variable name to lowercase
 	}
 
 
 	
-	public boolean isAvailable() {//Change the return type and getter method
-		return State == State.AVAILABLE;//Change the name of the enum 
+	public boolean isAvailable() {//Change the method name to be more meaningful
+		return state == State.AVAILABLE;//Change the name of the enum and the class variable based on the standard
 	}
 
 	
-	public boolean On_loan() {// 
-		return State == STATE.ON_LOAN;
+	public boolean isOnLoan() {//Change the method name to be more meaningful
+		return state == State.ON_LOAN;//Change the name of the enum and the class variable based on the standard
 	}
 
 	
-	public boolean isDamaged() {//Change the method name based on the standard
-		return State == State.DAMAGED;//Change the enum based on the standard
+	public boolean isDamaged() {//Change the method name to be more meaningful
+		return State == State.DAMAGED;//Change the name of the enum and the class variable based on the standard
 	}
 
 	
-	public void borrow() {//Change the method name based on the standard
-		if (State.equals(State.AVAILABLE)) {//Change the enum name based on the standard
-			State = State.ON_LOAN;//Change the enum name based on the standard
+	public void setBorrow() {//Change the method name to be more meaningful
+		if (state.equals(State.AVAILABLE)) {//Change the name of the enum and the class variable based on the standard
+			state = State.ON_LOAN;//Change the name of the enum and the class variable based on the standard
 		}
 		else {
-			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", State));
+			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", state));//Change the class variable based on the standard
 		}
 		
 	}
 
 
-	public void Return(boolean DAMAGED) {
-		if (State.equals(STATE.ON_LOAN)) {
+	public void setReturn(boolean DAMAGED) {//Change the method name to be more meaningful
+		if (state.equals(State.ON_LOAN)) {//Change the name of the enum and the class variable into lower case
 			if (DAMAGED) {
-				State = STATE.DAMAGED;
+				state = State.DAMAGED; //Change the name of the enum and the class variable into lowercase
 			}
 			else {
-				State = STATE.AVAILABLE;
+				state = State.AVAILABLE;//Change the name of the enum and the class variable into lower case
 			}
 		}
 		else {
-			throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", State));
+			throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", state));//Change the class variable into lowercase
 		}		
 	}
 
 	
-	public void Repair() {
-		if (State.equals(STATE.DAMAGED)) {
-			State = STATE.AVAILABLE;
+	public void setRepair() { //Change the method name to be more meaningful
+		if (state.equals(State.DAMAGED)) {//Change the name of the enum and the class variable into lowercase
+			state = State.AVAILABLE;//Change the name of the enum and the class variable into lowercase
 		}
 		else {
-			throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", State));
+			throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", state));//Change the class variable into lowercase
+		}		
 		}
 	}
 
