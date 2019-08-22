793edd85f2a11f034b8c2f93659ac2a5ff2d5964
rasiabc <52956030+rasiabc@users.noreply.github.com>
2019-08-22 17:06:40 +1000

    Update Library.java

--- ITC205_201960_Asg2_Baseline_Code/Library.java
+++ ITC205_201960_Asg2_Baseline_Code/Library.java
@@ -23,24 +23,47 @@ public class Library implements Serializable { // Class name should start with u
 	private static final double damageFee = 2.0;
 	
 	private static library self; //Variable name should be starts with lowercase and should be in camelback.
+	/*
+	Reviewed by Rasika - (22/09/2019)
+	Class name should be changed as Library
+	*/
 	private int bookId; //Variable name should be starts with lowercase and should be in camelback.
 	private int memberId; //Variable name should be starts with lowercase and should be in camelback.
 	private int loanId; //Variable name should be starts with lowercase and should be in camelback.
 	private Date loanDate; //Variable name should be starts with lowercase and should be in camelback.
 	
-	private Map<Integer, book> catalog; //Variable name should be starts with lowercase and should be in camelback.
-	private Map<Integer, member> members; //Variable name should be starts with lowercase and should be in camelback.
-	private Map<Integer, loan> loans; //Variable name should be starts with lowercase and should be in camelback.
-	private Map<Integer, loan> currentLoans; //Variable name should be starts with lowercase and should be in camelback.
-	private Map<Integer, book> damagedBooks; //Variable name should be starts with lowercase and should be in camelback.
-	
+	private Map<Integer, Book> catalog; //Variable name should be starts with lowercase and should be in camelback.
+	/*
+	Reviewed by Rasika - (22/09/2019)
+	Map value should be changed to Book
+	*/
+	private Map<Integer, Member> members; //Variable name should be starts with lowercase and should be in camelback.
+	/*
+	Reviewed by Rasika - (22/09/2019)
+	Map value should be changed to Member
+	*/
+	private Map<Integer, Loan> loans; //Variable name should be starts with lowercase and should be in camelback.
+	/*
+	Reviewed by Rasika - (22/09/2019)
+	Map value should be changed to Loan
+	*/
+	private Map<Integer, Loan> currentLoans; //Variable name should be starts with lowercase and should be in camelback.
+	/*
+	Reviewed by Rasika - (22/09/2019)
+	Map value should be changed to Loan
+	*/
+	private Map<Integer, Book> damagedBooks; //Variable name should be starts with lowercase and should be in camelback.
+	/*
+	Reviewed by Rasika - (22/09/2019)
+	Map value should be changed to Book
+	*/
 
 	private library() {
-		CATALOG = new HashMap<>();
-		MEMBERS = new HashMap<>();
-		LOANS = new HashMap<>();
-		CURRENT_LOANS = new HashMap<>();
-		DAMAGED_BOOKS = new HashMap<>();
+		catalog = new HashMap<>();//Changed variable as catalog - Reviewed by Rasika - (22/09/2019)
+		memeber = new HashMap<>();//Changed variable as member - Reviewed by Rasika - (22/09/2019)
+		loans = new HashMap<>();//Changed variable as loans - Reviewed by Rasika - (22/09/2019)
+		currentLoans = new HashMap<>();//Changed variable as currentLoans - Reviewed by Rasika - (22/09/2019)
+		damagedBooks = new HashMap<>();//Changed variable as damagedBooks - Reviewed by Rasika - (22/09/2019)
 		bookId = 1; //Variable name should be starts with lower case and should be in camelback.
 		memberId = 1; //Variable name should be starts with lower case and should be in camelback.	
 		loanId = 1; //Variable name should be starts with lower case and should be in camelback.
@@ -51,29 +74,30 @@ public class Library implements Serializable { // Class name should start with u
 		if (self == null) { //Variable name should be starts with lowercase and should be in camelback.
 			Path path = Paths.get(libraryFile);	//object should start with lower case	
 			if (Files.exists(path)) {	
-				try (ObjectInputStream LiF = new ObjectInputStream(new FileInputStream(libraryFile));) {
+				try (ObjectInputStream lif = new ObjectInputStream(new FileInputStream(libraryFile));) {//Variable LIF should be changed to lif - Reviewed by Rasika - (22/09/2019)
 			    
-					self = (library) LiF.readObject(); //Variable name should be starts with lowercase and should be in camelback.
+					self = (library) lif.readObject(); //Variable name should be starts with lowercase and should be in camelback.
 					Calendar.instance().setDate(self.loanDate); //Variable name should be starts with lowercase and should be in camelback and Method name should be start with lowercase and be camelback. Also underscore separated methods name not acceptable.
-					LiF.close();
+					lif.close();
 				}
 				catch (Exception e) {
 					throw new RuntimeException(e);
 				}
 			}
-			else self = new library(); //Variable name should be starts with lowercase and should be in camelback.
+			else self = new Library(); //Variable name should be starts with lowercase and should be in camelback.
+			//Class name should be changed as Library - Reviewed by Rasika - (22/09/2019)
 		}
 		return self;
 	}
 
 	
-	public static synchronized void SAVE() {
+	public static synchronized void SAVE() {//method name should be changed as save - Reviewed by Rasika - (22/09/2019)
 		if (self != null) { //Variable name should be starts with lowercase and should be in camelback.
 			instance.loanDate = Calendar.instance().Date(); //Variable name should be starts with lowercase and should be in camelback and method name should be start with lower case
-			try (ObjectOutputStream LoF = new ObjectOutputStream(new FileOutputStream(libraryFile));) {
-				LoF.writeObject(self); //Variable name should be starts with lowercase and should be in camelback.
-				LoF.flush();
-				LoF.close();	
+			try (ObjectOutputStream lof = new ObjectOutputStream(new FileOutputStream(libraryFile));) {
+				lof.writeObject(self); //Variable name should be starts with lowercase and should be in camelback.
+				lof.flush();
+				lof.close();	
 			}
 			catch (Exception e) {
 				throw new RuntimeException(e);
@@ -107,45 +131,49 @@ public class Library implements Serializable { // Class name should start with u
 	}
 
 	
-	public List<member> MEMBERS() {		
-		return new ArrayList<member>(MEMBERS.values()); 
+	public List<Member> member() {		//Method should be members() -Reviewed by Rasika - (22/09/2019) 
+		return new ArrayList<member>(member.values()); //variable should be member - Reviewed by Rasika - (22/09/2019)
 	}
 
 
-	public List<book> BOOKS() {		
-		return new ArrayList<book>(catalog.values()); 
+	public List<Book> books() {	//Method should be books() -Reviewed by Rasika - (22/09/2019)	
+		return new ArrayList<Book>(catalog.values()); //ArrayList type should be Book - Reviewed by Rasika - (22/09/2019)
 	}
 
 
-	public List<loan> CurrentLoans() {
-		return new ArrayList<loan>(currentLoans.values());
+	public List<Loan> currentLoans() {//Method should be currentLoans() -Reviewed by Rasika - (22/09/2019)	
+		return new ArrayList<Loan>(currentLoans.values());
 	}
 
 
-	public member Add_mem(String lastName, String firstName, String email, int phoneNo) {		
-		member member = new member(lastName, firstName, email, phoneNo, NextMID());
-		MEMBERS.put(member.getId(), member);	//method should be starts with lowercase and should be in camelback. 	
+	public Member addMember(String lastName, String firstName, String email, int phoneNo) {	
+		/*
+		return typr should be Member
+		method name should be addMember
+		*/
+		Member member = new Member(lastName, firstName, email, phoneNo, NextMID());//Class name should be Member - Reviewed by Rasika - (22/09/2019)
+		members.put(member.getId(), member);	//method should be starts with lowercase and should be in camelback. 	
 		return member;
 	}
 
 	
-	public book Add_book(String a, String t, String c) {		
-		book book = new book(a, t, c, nextBookId());  //method should be starts with lowercase and should be in camelback and should be meaningful, object name should be meaningful
+	public Book addBbook(String a, String t, String c) {//Method name should be addBook - Reviewed by Rasika - (22/09/2019)	
+		Book book = new Book(a, t, c, nextBookId());  //method should be starts with lowercase and should be in camelback and should be meaningful, object name should be meaningful
 		catalog.put(book.ID(), book);		
 		return book;
 	}
 
 	
-	public member MEMBER(int memberId) {
+	public Member member(int memberId) {
 		if (members.containsKey(memberId)) //Variable name should be starts with lowercase and should be in camelback.
 			return members.get(memberId);//Variable name should be starts with lowercase and should be in camelback.
 		return null;
 	}
 
 	
-	public book Book(int bookId) {
-		if (CATALOG.containsKey(bookId)) 
-			return CATALOG.get(bookId);		
+	public Book book(int bookId) {
+		if (catalog.containsKey(bookId)) 
+			return catalog.get(bookId);		
 		return null;
 	}
 
@@ -155,14 +183,14 @@ public class Library implements Serializable { // Class name should start with u
 	}
 
 	
-	public boolean memberCanBorrow(member member) {	//method should be starts with lowercase and should be in camelback	
+	public boolean memberCanBorrow(Member member) {	//method should be starts with lowercase and should be in camelback	
 		if (member.numberOfCurrentLoans() == loanLimit ) //method should be starts with lowercase and should be in camelback
 			return false;
 				
-		if (member.Fines_OwEd() >= maxFinesOwed) 
+		if (member.finesOwed() >= maxFinesOwed) 
 			return false;
 				
-		for (loan loan : member.getLoans()) //method should be starts with lowercase and should be in camelback
+		for (Loan loan : member.getLoans()) //method should be starts with lowercase and should be in camelback
 			if (loan.overDue()) //method should be starts with lowercase and should be in camelback
 				return false;
 			
@@ -170,14 +198,14 @@ public class Library implements Serializable { // Class name should start with u
 	}
 
 	
-	public int loansRemainingForMembers(member member) {	 //method should be starts with lowercase and should be in camelback	
+	public int loansRemainingForMembers(Member member) {	 //method should be starts with lowercase and should be in camelback	
 		return loanLimit - member.numberOfCurrentLoans(); //method should be starts with lowercase and should be in camelback
 	}
 
 	
-	public loan issueLoan(book book, member member) { //method should be starts with lowercase and should be in camelback
-		Date dueDate = Calendar.instance().Due_Date(loanPeriod); //method should be starts with lowercase and should be in camelback
-		loan loan = new loan(nextLoanID(), book, member, dueDate); //method should be starts with lowercase and should be in camelback
+	public Loan issueLoan(Book book, Member member) { //method should be starts with lowercase and should be in camelback
+		Date dueDate = calendar.instance().dueDate(loanPeriod); //method should be starts with lowercase and should be in camelback
+		Loan loan = new loan(nextLoanID(), book, member, dueDate); //method should be starts with lowercase and should be in camelback
 		member.takeOutLoan(loan); //method should be starts with lowercase and should be in camelback
 		book.borrow(); //method should be starts with lowercase and should be in camelback
 		loans.put(loan.ID(), loan); 
@@ -186,7 +214,7 @@ public class Library implements Serializable { // Class name should start with u
 	}
 	
 	
-	public loan LOAN_BY_BOOK_ID(int bookId) { 
+	public loan loanByBookID(int bookId) { 
 		if (currentLoans.containsKey(bookId)) { //Variable name should be starts with lowercase and should be in camelback.
 			return currentloans.get(bookId); //Variable name should be starts with lowercase and should be in camelback.
 		}
@@ -194,7 +222,7 @@ public class Library implements Serializable { // Class name should start with u
 	}
 
 	
-	public double calculateOverDueFine(loan loan) {  //method should be starts with lowercase
+	public double calculateOverDueFine(Loan loan) {  //method should be starts with lowercase
 		if (loan.overDue()) { //method should be starts with lowercase and should be in camelback
 			long daysOverDue = Calendar.instance().getDaysDifference(loan.getDueDate()); //method should be starts with lowercase and should be in camelback
 			double fine = daysOverDue * finePerDay;
@@ -204,34 +232,34 @@ public class Library implements Serializable { // Class name should start with u
 	}
 
 
-	public void dischargeLoan(loan currentLoan, boolean isDamaged) { //method should be starts with lowercase and should be in camelback
-		member member = currentLoan.Member();
-		book book  = currentLoan.Book();
+	public void dischargeLoan(Loan currentLoan, boolean isDamaged) { //method should be starts with lowercase and should be in camelback
+		Member member = currentLoan.member();
+		Book book  = currentLoan.Book();
 		
 		double overDueFine = calculateOverDueFine(currentLoan); //method should be starts with lowercase
 		member.addFine(overDueFine); //method should be starts with lowercase and should be in camelback	
 		
 		member.dishchargeLoane(currentLoan); //method should be starts with lowercase and should be in camelback
-		book.Return(isDamaged);
+		book.return(isDamaged);
 		if (isDamaged) {
 			member.addFine(damageFee); //method should be starts with lowercase and should be in camelback
-			damagedBooks.put(book.ID(), book); //Variable name should be starts with lowercase and should be in camelback.
+			damagedBooks.put(book.id(), book); //Variable name should be starts with lowercase and should be in camelback.
 		}
 		currentLoan.discharge(); //method should be starts with lowercase and should be in camelback
-		currentLoans.remove(book.ID()); //Variable name should be starts with lowercase and should be in camelback.
+		currentLoans.remove(book.id()); //Variable name should be starts with lowercase and should be in camelback.
 	}
 
 
 	public void checkCurrentLoans() {
-		for (loan loan : currentLoans.values()) { //Variable name should be starts with lowercase and should be in camelback.
+		for (Loan loan : currentLoans.values()) { //Variable name should be starts with lowercase and should be in camelback.
 			loan.checkOverDue();
 		}		
 	}
 
 
-	public void repairBook(book currentBook) { //method should be starts with lowercase and should be in camelback
+	public void repairBook(Book currentBook) { //method should be starts with lowercase and should be in camelback
 		if (damagedBooks.containsKey(currentBook.ID())) {//Variable name should be starts with lowercase and should be in camelback.
-			currentBook.Repair();
+			currentBook.repair();
 			damagedBooks.remove(currentBook.ID());//Variable name should be starts with lowercase and should be in camelback.
 		}
 		else {
